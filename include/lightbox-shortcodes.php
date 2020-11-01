cf024d44ac5b96a3792b1df1ee50a377b346570d
Garathd <garath.davis.gti@gmail.com>
2020-11-01 20:03:23 +0000

    Good Working Demo, still alot to do with regards to responsive styles but can now use multiple tooltips

--- include/lightbox-shortcodes.php
+++ include/lightbox-shortcodes.php
@@ -1,3 +1,65 @@
 <?php
+// ===============================================================
+// lightbox-shortcodes.php
+//
+// Shortcode usage - WP editors can add the shortcodes       
+// ----------------------------------------------------------------
+
+add_shortcode('gdx_lightbox', 'lightbox_init');
+
+function lightbox_init($atts) { 
+
+  $atts = shortcode_atts( 
+    array(
+      'img' => '',
+      'id' => ''
+    ), 
+    $atts
+  );
+
+    if ( ! empty( $atts['img'] ) ) {
+        $img = $atts['img'];
+    }
+
+    if ( ! empty( $atts['id'] ) ) {
+        $id = $atts['id'];
+    }
+
+    
+
+      $tooltip_img = WP_PLUGIN_URL . '/gdx-lightbox-tooltip/include/assets/img/info.png';
+      //$construct .= '<img src="'.$img.'">';
+      
+
+      $construct = '<div class="gdx-lightbox-container">';
+      $construct .= '<div class="gdx-btn-group">';
+      $construct .= '  <a type="button" class="gdx-open-modal" data-open="modal'.$id.'">';
+      $construct .= '  <img src="'.$tooltip_img.'">';
+      $construct .= '  </a>';
+      $construct .= '</div>';
+      $construct .= '</div>';
+    
+    
+      $construct .= '<div class="gdx-modal" id="modal'.$id.'" data-animation="slideInOutLeft">';
+      $construct .= '<div class="gdx-modal-dialog">';
+      $construct .= '<header class="gdx-modal-header">';
+      $construct .= '   <a class="gdx-close-modal" aria-label="close modal" data-close>';
+      $construct .= '✕';
+      $construct .= '   </a>';
+      $construct .= '  </header>';
+      $construct .= '  <section class="gdx-modal-content">';
+      $construct .= '<img src="'.$img.'">';
+      $construct .= '  </section>';
+      $construct .= '  <footer class="gdx-modal-footer">';
+      $construct .= '  </footer>';
+      $construct .= ' </div>';
+      $construct .= ' </div>';
+    
+    
+      
+      return $construct; 
+
+
+} 
 
 ?>
