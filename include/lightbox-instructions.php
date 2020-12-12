13fb68f8bacca30a94b5be941d280aafd811afb7
Garathd <garath.davis.gti@gmail.com>
2020-12-12 21:34:26 +0000

    Added tooltips to shortcode and created new open close shortcode to embed html and text and updated the css

--- include/lightbox-instructions.php
+++ include/lightbox-instructions.php
@@ -10,23 +10,44 @@
     .lightbox-instructions strong {
         font-size: 1.2em !important;      
     }
+    .gdx-open-modal img {
+        margin: -0.3em;
+        margin-left: 0.25em;
+    }
 </style>
 
 <div class="lightbox-instructions">
 	<div class="lightbox-tooltip-instructions-header">
 		<h2 class="instructions-heading" id="use-shortcodes">How to use (Shortcodes):</h2>
 
-		<p>To use Lightbox Tooltip on a page you must first use the below shortcode</p>
+		<p>To use Lightbox Tooltip on a page you must first use the below shortcode:</p>
         <p><strong><xmp>[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" id="1"]</xmp></strong></p>
+        <hr />
         &nbsp;
-        <p>Make sure to always use a different id as well for each individual image</p>
+        <p>Make sure to always use a different id as well for each individual image:</p>
         <p><strong><xmp>[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/little-frog.jpg" id="2"]</xmp></strong></p>
+        <hr />
 		&nbsp;
-        <p>To use with text instead of the information icon</p>
+        <p>To use with text instead of the information icon:</p>
         <p><strong><xmp>[gdx_lightbox text="Little Frog Text" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="3"]</xmp></strong></p>
+        <hr />
 		&nbsp;
-         <p>To use with animation</p>
-        <p><strong><xmp>[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutDown" id="4"]</xmp></strong></p>
+        <p>Little Frog with Custom Icon:</p>
+        <p><strong><xmp>[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="4" tooltip="https://gdxdesigns.com/wp-content/uploads/2020/12/tooltip.png"]</xmp></strong></p>
+        <hr />
+        &nbsp;
+        <p>To use with animation:</p>
+        <p><strong><xmp>[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutDown" id="5"]</xmp></strong></p>
+        <hr />
+        &nbsp;
+        <p>To use with HTML/Text embeded inside Lightbox:</p>
+        <p><strong><xmp>[gdx_lightbox_text id="12" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg"]</xmp></strong>
+
+        <p style="margin-left: 1%;">Enter text here and images or any html inside. All the standard shortcode attributes still work with this shortcode.</p>
+
+        <strong><xmp>[/gdx_lightbox_text]</xmp></strong>
+        </p>
+        <hr />
         &nbsp;
         <h3>Animations List</h3>
         <ul>
@@ -38,12 +59,24 @@
         	<li>rotateInOutDown</li>
         	<li>mixInAnimations</li>
         </ul>
-
+        &nbsp;
+        <hr />
+        <h3>Attributes List</h3>
+        <ul>
+            <li>text</li>
+            <li>tooltip</li>
+            <li>img</li>
+            <li>animate</li>
+            <li>title</li>
+            <li>id</li>
+        </ul>
+        &nbsp;
+        <hr />
 	</div>
 	&nbsp;
 	<div class="lightbox-tooltip-instructions-content">
 		<h2 class="instructions-heading" id="code-example">Code Example:</h2>
-        &nbsp;
+        
         <h3>Basic</h3>
 
 		<xmp>
@@ -55,60 +88,89 @@
         </xmp>
 
         <xmp>
-        <p>Here is the [gdx_lightbox text="Little Frog Text" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="3"]</p>
+        <p>Here is the: [gdx_lightbox text="Little Frog Text" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="3"]</p>
         </xmp>
+
+        <xmp>
+        <p>Little Frog with Custom Icon: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="4" tooltip="https://gdxdesigns.com/wp-content/uploads/2020/12/tooltip.png"]</p>
+        </xmp>
+        <hr />
+
+        <h3>Advanced</h3>
+
+        <xmp>
+        <p>Embed HTML and Text Content: [gdx_lightbox_text id="12" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg"]
+
+        Enter text here and images or any html inside. All the standard shortcode attributes still work with this shortcode.
+
+        [/gdx_lightbox_text]</p>
+
+        </xmp>
+        <hr />
 
         <h3>Animations</h3>
 
         <xmp>
-        <p>Frog Shadow Info Icon slideInOutDown: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutDown" id="4"]</p>
+        <p>Frog Shadow Info Icon slideInOutDown: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutDown" id="5"]</p>
         </xmp>
 
         <xmp>
-        <p>Frog Shadow Info Icon slideInOutTop: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutTop" id="5"]</p>
+        <p>Frog Shadow Info Icon slideInOutTop: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutTop" id="6"]</p>
         </xmp>
 
         <xmp>
-        <p>Frog Shadow Info Icon slideInOutLeft: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutLeft" id="6"]</p>
+        <p>Frog Shadow Info Icon slideInOutLeft: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutLeft" id="7"]</p>
         </xmp>
 
         <xmp>
-        <p>Frog Shadow Info Icon slideInOutRight with title: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutRight" title="Frog Image" id="7"]</p>
+        <p>Frog Shadow Info Icon slideInOutRight with title: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutRight" title="Frog Image" id="8"]</p>
         </xmp>
 
         <xmp>
-        <p>Frog Shadow Info Icon zoomInOut: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="zoomInOut" id="8"]</p>
+        <p>Frog Shadow Info Icon zoomInOut: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="zoomInOut" id="9"]</p>
         </xmp>
 
         <xmp>
-        <p>Frog Shadow Info Icon rotateInOutDown: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="rotateInOutDown" id="9"]</p>
+        <p>Frog Shadow Info Icon rotateInOutDown: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="rotateInOutDown" id="10"]</p>
         </xmp>
 
         <xmp>
-        <p>Frog Shadow Info Icon mixInAnimations: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="mixInAnimations" id="10"]</p>
+        <p>Frog Shadow Info Icon mixInAnimations: [gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="mixInAnimations" id="11"]</p>
         </xmp>
+        <hr />
 	</div>
 
 	 &nbsp;
 	 <h2 class="instructions-heading" id="code-preview">Code Preview:</h2>
+
+     <h3 id="code-preview">Basic:</h3>
 	 <p>Frog Shadow Info Icon: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" id="1"]'); ?></p>
-	&nbsp;
+	 &nbsp;
      <p>Little Frog Info Icon: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/little-frog.jpg" id="2"]'); ?></p>
-	&nbsp;
-     <p>Here is the <?php echo do_shortcode('[gdx_lightbox text="Little Frog Text" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="3"]'); ?></p>
-	&nbsp;
+	 &nbsp;
+     <p>Here is the: <?php echo do_shortcode('[gdx_lightbox text="Little Frog Text" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="3"]'); ?></p>
+     &nbsp;
+     <p>Little Frog with Custom Icon: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-hand.jpg" id="4" tooltip="https://gdxdesigns.com/wp-content/uploads/2020/12/tooltip.png"]'); ?></p>
+	 &nbsp;
+     <hr />
+     <h3 id="code-preview">Advanced:</h3>
+     <p>Embed HTML and Text Content: <?php echo do_shortcode('[gdx_lightbox_text id="12" img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg"]'."<p>Enter text here and images or any html inside. All the standard shortcode attributes still work with this shortcode.</p>".'[/gdx_lightbox_text]'); ?></p>
+     &nbsp;
+     <hr />
      <h3 id="code-preview">Animations:</h3>
 
-     <p>Frog Shadow Info Icon slideInOutDown: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutDown" id="4"]'); ?></p>
+     <p>Frog Shadow Info Icon slideInOutDown: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutDown" id="5"]'); ?></p>
 	&nbsp;
-     <p>Frog Shadow Info Icon slideInOutTop: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutTop" id="5"]'); ?></p>
+     <p>Frog Shadow Info Icon slideInOutTop: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutTop" id="6"]'); ?></p>
 	&nbsp;
-     <p>Frog Shadow Info Icon slideInOutLeft: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutLeft" id="6"]'); ?></p>
+     <p>Frog Shadow Info Icon slideInOutLeft: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutLeft" id="7"]'); ?></p>
 	&nbsp;
-     <p>Frog Shadow Info Icon slideInOutRight with title: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutRight" title="Frog Image" id="7"]'); ?></p>
+     <p>Frog Shadow Info Icon slideInOutRight with title: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="slideInOutRight" title="Frog Image" id="8"]'); ?></p>
 	&nbsp;
-     <p>Frog Shadow Info Icon zoomInOut: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="rotateInOutDown" id="9"]'); ?></p>
+     <p>Frog Shadow Info Icon zoomInOut: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="zoomInOut" id="9"]'); ?></p>
+    &nbsp;
+     <p>Frog Shadow Info Icon rotateInOutDown: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="rotateInOutDown" id="9"]'); ?></p>
 	&nbsp;
-     <p>Frog Shadow Info Icon mixInAnimations: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="mixInAnimations" id="10"]'); ?></p>
+     <p>Frog Shadow Info Icon mixInAnimations: <?php echo do_shortcode('[gdx_lightbox img="https://gdxdesigns.com/wp-content/uploads/2020/11/frog-shadow.jpg" animate="mixInAnimations" id="11"]'); ?></p>
 
 </div>
--- include/lightbox-shortcodes.php
+++ include/lightbox-shortcodes.php
@@ -12,6 +12,7 @@ function lightbox_init($atts) {
   $atts = shortcode_atts( 
     array(
       'text' => '',
+      'tooltip' => '',
       'img' => '',
       'animate' => '',
       'title' => '',
@@ -24,33 +25,42 @@ function lightbox_init($atts) {
         $text = $atts['text'];
     }
 
+    if ( ! empty( $atts['tooltip'] ) ) {
+        $tooltip = $atts['tooltip'];
+    }
+
     if ( ! empty( $atts['img'] ) ) {
         $img = $atts['img'];
     }
 
     if ( ! empty( $atts['animate'] ) ) {
         $animate = $atts['animate'];
-    } else {
-      $animate = "slideInOutLeft";
+    } 
+    else {
+        $animate = "slideInOutLeft";
     }
 
     if ( ! empty( $atts['title'] ) ) {
-      $title = $atts['title'];
+        $title = $atts['title'];
     }
 
     if ( ! empty( $atts['id'] ) ) {
         $id = $atts['id'];
     }
 
-    
-
-      $tooltip_img = WP_PLUGIN_URL . '/gdx-lightbox-tooltip/include/assets/img/info.png';
-      //$construct .= '<img src="'.$img.'">';
+      $default_tooltip_img = WP_PLUGIN_URL . '/gdx-lightbox-tooltip/include/assets/img/info.png';
       
-      $construct .= '  <a type="button" id="gdx-lighbox-modal-unique-'.$id.'" class="gdx-open-modal lightbox-link" data-open="gdx-lighbox-modal-'.$id.'">';
+      $construct = '  <a type="button" id="gdx-lighbox-modal-unique-'.$id.'" class="gdx-open-modal lightbox-link" data-open="gdx-lighbox-modal-'.$id.'">';
 
-      if(!$text){
-        $construct .= '<img src="'.$tooltip_img.'">';
+      if(!$text && !$tooltip){
+        $construct .= '<img src="'.$default_tooltip_img.'">';
+      } 
+
+      else {
+        // Only Show If Tooltip is Defined
+        if($tooltip) {
+          $construct .= '<img src="'.$tooltip.'">';
+        }
       }
 
       if($text){
@@ -76,8 +86,83 @@ function lightbox_init($atts) {
       $construct .= ' </div>';
     
       return $construct; 
-
-
 } 
+
+add_shortcode('gdx_lightbox_text', 'lightbox_advanced');
+
+function lightbox_advanced($atts = array(), $content = null ) { 
+
+  if ( ! empty( $atts['text'] ) ) {
+      $text = $atts['text'];
+  }
+
+  if ( ! empty( $atts['tooltip'] ) ) {
+        $tooltip = $atts['tooltip'];
+  }
+
+  if ( ! empty( $atts['img'] ) ) {
+      $img = $atts['img'];
+  }
+
+  if ( ! empty( $atts['animate'] ) ) {
+      $animate = $atts['animate'];
+  } 
+  
+  else {
+    $animate = "slideInOutLeft";
+  }
+
+  if ( ! empty( $atts['title'] ) ) {
+        $title = $atts['title'];
+  } 
+
+  if ( ! empty( $atts['id'] ) ) {
+        $id = $atts['id'];
+  } 
+
+  
+  /*Contruction*/
+
+  $default_tooltip_img = WP_PLUGIN_URL . '/gdx-lightbox-tooltip/include/assets/img/info.png';
+  //$construct .= '<img src="'.$img.'">';
+      
+  $lightbox_data = '<a type="button" id="gdx-lighbox-modal-unique-'.$id.'" class="gdx-open-modal lightbox-link" data-open="gdx-lighbox-modal-'.$id.'">';
+
+  if(!$text && !$tooltip){
+    $lightbox_data .= '<img src="'.$default_tooltip_img.'">';
+  } 
+
+  else {
+    $lightbox_data .= '<img src="'.$tooltip.'">';
+  }
+
+  if($text){
+    $lightbox_data .= $text;
+  }
+
+      $lightbox_data .= '  </a>';
+    
+      $lightbox_data .= '<div class="gdx-modal" id="gdx-lighbox-modal-'.$id.'" data-animation="'.$animate.'">';
+      $lightbox_data .= '<div class="gdx-modal-dialog">';
+      $lightbox_data .= '<header class="gdx-modal-header">';
+      $lightbox_data .= '   <a class="gdx-close-modal" aria-label="close modal" data-close>';
+      $lightbox_data .= '✕';
+      $lightbox_data .= '   </a>';
+      $lightbox_data .= '  </header>';
+      $lightbox_data .= '  <section class="gdx-modal-content">';
+      if($img) {
+        $lightbox_data .= '<img src="'.$img.'">';
+      }
+      $lightbox_data .= $content;
+      $lightbox_data .= '  </section>';
+      $lightbox_data .= '  <footer class="gdx-modal-footer">';
+      $lightbox_data .= '  <h3 class="gdx-modal-image-title">'.$title.'</h3>';
+      $lightbox_data .= '  </footer>';
+      $lightbox_data .= ' </div>';
+      $lightbox_data .= ' </div>';
+    
+  return $lightbox_data;
+} 
+
 
 ?>
